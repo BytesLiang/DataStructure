1eb47a15e64ad10c7710cc037ebd1d1beb5ceea0
BytesLiang <xinzhu_liang@163.com>
2021-08-23 11:37:32 +0800

    test

--- introduction/learn.cpp
+++ introduction/learn.cpp
@@ -1,16 +1,21 @@
 /* 自学C++语言 */
 #include "complex.h"
 #include <iostream>
-
+#include <iterator>
+#include <string>
+#include <vector>
 using namespace std;
 
+void error_msg(initializer_list<string> li,int i)
+{
+    cout << i;
+    for(const auto& elem:li)
+        cout << elem << " ";
+    cerr << __func__;
+}
 int main()
 {
-    int n = 5;
-    while (--n)
-    {
-        cout << n;
-    }
+    error_msg({"wo", "shi", "nd"}, 5);
     getchar();
     getchar();
     return 0;
